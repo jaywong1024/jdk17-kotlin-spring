08c8808ec9c8a28c70935112bf0dad25da0b750c
Jay <huanghj@dragonpass.com.cn>
2023-10-17 16:58:45 +0800

    Initial commit

---
+++ src/main/kotlin/io/github/jaywong1024/MainApp.kt
@@ -0,0 +1,23 @@
+package io.github.jaywong1024
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@SpringBootApplication
+open class MainApp {
+
+    @RequestMapping("/")
+    fun home() = "Fuck you! Java!"
+
+}
+
+fun main(args: Array<String>) {
+    var len = args?.size;
+    runApplication<MainApp>(*args)
+}
+
+
+
